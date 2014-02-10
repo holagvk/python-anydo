9786191e3cfca35498df38a08257e85360061e65
Gaurav Kalra <gvkalra@gmail.com>
2014-02-11 02:52:35 +0530

    Adding exception classes

---
+++ anydo/error.py
@@ -0,0 +1,15 @@
+class AnyDoClientError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
+
+
+class AnyDoAPIError(Exception):
+    def __init__(self, code, msg):
+        self.code = code
+        self.msg = msg
+
+    def __str__(self):
+        return "(%s): %s" % (self.code, self.msg)
