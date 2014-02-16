10a593dcf4ea926d067f9857c3454c451772c283
Kouhei Maeda <mkouhei@palmtb.net>
2014-02-17 07:34:20 +0900

    Adding classifiers to setup.py
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- setup.py
+++ setup.py
@@ -2,6 +2,18 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
+classifiers = [
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3.2",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
+    "Topic :: Internet",
+    "Topic :: Internet :: WWW/HTTP",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
+
 setup(name="python-anydo",
       version="0.1.0",
       description="Unoffical Any.Do API client",
@@ -10,6 +22,7 @@ setup(name="python-anydo",
       author="Gaurav Kalra",
       author_email="gvkalra@gmail.com",
       url="https://github.com/gvkalra/python-anydo",
+      classifiers=classifiers,
       packages=find_packages(),
       keywords="anydo",
       test_suite='anydo.tests',
