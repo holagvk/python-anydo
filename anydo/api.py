c83c55f7b610a4fea8476407d8204d5d9a598d90
Gaurav Kalra <gaurav.kalra@samsung.com>
2014-02-27 15:33:16 +0530

    pylint: Fixing invalid-name in API module

--- anydo/api.py
+++ anydo/api.py
@@ -23,98 +23,99 @@ class AnyDoAPI(object):
 
             Returns:
                 A dictionary of following user information:
-                {'anonymous': Boolean,
-                'creationDate': Number,
-                'email': String,
-                'emails': Array,
-                'facebookAccessToken': String,
-                'facebookId': String,
-                'fake': Boolean,
-                'id': String,
-                'name': String,
-                'phoneNumbers': Array}
+                    {'anonymous': Boolean,
+                    'creationDate': Number,
+                    'email': String,
+                    'emails': Array,
+                    'facebookAccessToken': String,
+                    'facebookId': String,
+                    'fake': Boolean,
+                    'id': String,
+                    'name': String,
+                    'phoneNumbers': Array}
 
             Raises:
                 AnyDoAPIError:
                     Code(420): JSON Decoding Error.
         """
-        r = self.api.user_info()
+        ret = self.api.user_info()
         try:
-            return r.json()
+            return ret.json()
         except ValueError:
             raise AnyDoAPIError(420, "JSON Decoding Error")
 
     def get_all_tasks(self, response_type="flat",
                       include_deleted=False,
                       include_done=False):
-        r = self.api.tasks(response_type,
-                           include_deleted,
-                           include_done)
+        ret = self.api.tasks(response_type,
+                             include_deleted,
+                             include_done)
         try:
-            return r.json()
+            return ret.json()
         except ValueError:
             raise AnyDoAPIError(420, "JSON Decoding Error")
 
     def get_all_categories(self, response_type="flat",
                            include_deleted=False,
                            include_done=False):
-        r = self.api.categories(response_type,
-                                include_deleted,
-                                include_done)
+        ret = self.api.categories(response_type,
+                                  include_deleted,
+                                  include_done)
         try:
-            return r.json()
+            return ret.json()
         except ValueError:
             raise AnyDoAPIError(420, "JSON Decoding Error")
 
     def get_task_by_id(self, task_id):
-        r = self.api.task(uuid=task_id)
+        ret = self.api.task(uuid=task_id)
         try:
-            return r.json()
+            return ret.json()
         except ValueError:
             raise AnyDoAPIError(420, "JSON Decoding Error")
 
     def delete_task_by_id(self, task_id):
-        r = self.api.delete_task(uuid=task_id)
-        if r.status_code != 204:
-            raise AnyDoAPIError(421, "HTTP Error %d" % r.status_code)
+        ret = self.api.delete_task(uuid=task_id)
+        if ret.status_code != 204:
+            raise AnyDoAPIError(421, "HTTP Error %d" % ret.status_code)
 
     def delete_category_by_id(self, category_id):
         if category_id == self.def_category_id:
             raise AnyDoAPIError(422, "Invalid Operation")
-        r = self.api.delete_category(uuid=category_id)
-        if r.status_code != 204:
-            raise AnyDoAPIError(421, "HTTP Error %d" % r.status_code)
+        ret = self.api.delete_category(uuid=category_id)
+        if ret.status_code != 204:
+            raise AnyDoAPIError(421, "HTTP Error %d" % ret.status_code)
 
     def create_new_category(self, category_name,
                             default=False,
-                            listPosition='null'):
-        r = self.api.create_category(category_name,
-                                     default,
-                                     isDefault="true" if default else "false",
-                                     listPosition=listPosition,
-                                     id=create_uuid())
+                            list_position='null'):
+        ret = self.api.create_category(category_name,
+                                       default,
+                                       isDefault="true" if default
+                                       else "false",
+                                       listPosition=list_position,
+                                       id=create_uuid())
         try:
-            return r.json()
+            return ret.json()
         except ValueError:
             raise AnyDoAPIError(420, "JSON Decoding Error")
 
     def create_new_task(self, task_title, due_day='someday'):
         try:
-            r = self.api.create_task(task_title,
-                                     listPositionByCategory=0,
-                                     listPositionByPriority=0,
-                                     listPositionByDueDate=0,
-                                     status="UNCHECKED",
-                                     repeatingMethod="TASK_REPEAT_OFF",
-                                     shared="false",
-                                     priority="Normal",
-                                     creationDate=int(time.time()),
-                                     taskExpanded=False,
-                                     categoryId=self.def_category_id,
-                                     dueDate={'someday': None,
-                                              'today': 0}[due_day],
-                                     id=create_uuid())
-            return r.json()
+            ret = self.api.create_task(task_title,
+                                       listPositionByCategory=0,
+                                       listPositionByPriority=0,
+                                       listPositionByDueDate=0,
+                                       status="UNCHECKED",
+                                       repeatingMethod="TASK_REPEAT_OFF",
+                                       shared="false",
+                                       priority="Normal",
+                                       creationDate=int(time.time()),
+                                       taskExpanded=False,
+                                       categoryId=self.def_category_id,
+                                       dueDate={'someday': None,
+                                                'today': 0}[due_day],
+                                       id=create_uuid())
+            return ret.json()
         except ValueError:
             raise AnyDoAPIError(420, "JSON Decoding Error")
         except KeyError:
