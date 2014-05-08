beb52c0b1e6a01dad38be37085935b930b7901d2
Kouhei Maeda <mkouhei@palmtb.net>
2014-05-08 12:15:56 +0900

    Change mock to httpretty for mock test.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- anydo/lib/tests/test_lib.py
+++ anydo/lib/tests/test_lib.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import unittest
-from mock import patch
+from httpretty import HTTPretty, httprettified
 import time
 from anydo.lib.bind import AnyDoAPIBinder
 from anydo.lib import settings
@@ -16,78 +16,91 @@ class TestAnyDoAPIBinder(AnyDoAPIBinder):
 class AnyDoAPIBinderTests(unittest.TestCase):
     setup_done = False  # #TODO: setUpClass ?
 
+    @httprettified
     def setUp(self):
         if not self.setup_done:
+            HTTPretty.register_uri(
+                HTTPretty.POST,
+                '%s/j_spring_security_check' % AnyDoAPIBinder.host)
             super(AnyDoAPIBinderTests, self).setUp()
             self.__class__.api = TestAnyDoAPIBinder(username=settings.USERNAME,
                                                     password=settings.PASSWORD)
             self.__class__.setup_done = True
 
-    @patch('requests.adapters.HTTPAdapter.send')
-    def test_user_info(self, mock_class):
+    @httprettified
+    def test_user_info(self):
+        HTTPretty.register_uri(HTTPretty.GET,
+                               '%s/me' % AnyDoAPIBinder.host)
         ret = self.api.user_info()
-        ret.status_code = 200
-        mock_class.return_value = ret
         self.assertEqual(ret.status_code, 200)
 
-    @patch('requests.adapters.HTTPAdapter.send')
-    def test_tasks(self, mock_class):
+    @httprettified
+    def test_tasks(self):
+        HTTPretty.register_uri(HTTPretty.GET,
+                               '%s/me/tasks' % AnyDoAPIBinder.host)
         ret = self.api.tasks(responseType="flat",
                              includeDeleted="false",
                              includeDone="false")
-        ret.status_code = 200
-        mock_class.return_value = ret
         self.assertEqual(ret.status_code, 200)
 
-    @patch('requests.adapters.HTTPAdapter.send')
-    def test_categories(self, mock_class):
+    @httprettified
+    def test_categories(self):
+        HTTPretty.register_uri(HTTPretty.GET,
+                               '%s/me/categories' % AnyDoAPIBinder.host)
         ret = self.api.categories(responseType="flat",
                                   includeDeleted="false",
                                   includeDone="false")
-        ret.status_code = 200
-        mock_class.return_value = ret
         self.assertEqual(ret.status_code, 200)
 
-    @patch('requests.adapters.HTTPAdapter.send')
-    def test_task(self, mock_class):
+    @httprettified
+    def test_task(self):
         task_id = utils.create_uuid()
+        HTTPretty.register_uri(HTTPretty.GET,
+                               '%s/me/tasks/%s' % (AnyDoAPIBinder.host,
+                                                   task_id))
         ret = self.api.task(uuid=task_id)
-        ret.status_code = 200
-        mock_class.return_value = ret
         self.assertEqual(ret.status_code, 200)
 
-    @patch('requests.adapters.HTTPAdapter.send')
-    def test_delete_task(self, mock_class):
+    @httprettified
+    def test_delete_task(self):
         task_id = utils.create_uuid()
+        HTTPretty.register_uri(HTTPretty.DELETE,
+                               '%s/me/tasks/%s' % (AnyDoAPIBinder.host,
+                                                   task_id),
+                               status=204)
         ret = self.api.delete_task(uuid=task_id)
-        ret.status_code = 204
-        mock_class.return_value = ret
         self.assertEqual(ret.status_code, 204)
 
-    @patch('requests.adapters.HTTPAdapter.send')
-    def test_delete_category(self, mock_class):
+    @httprettified
+    def test_delete_category(self):
         category_id = utils.create_uuid()
+        HTTPretty.register_uri(HTTPretty.DELETE,
+                               '%s/me/categories/%s' % (AnyDoAPIBinder.host,
+                                                        category_id),
+                               status=204)
         ret = self.api.delete_category(uuid=category_id)
-        ret.status_code = 204
-        mock_class.return_value = ret
         self.assertEqual(ret.status_code, 204)
 
-    @patch('requests.adapters.HTTPAdapter.send')
-    def test_create_category(self, mock_class):
+    @httprettified
+    def test_create_category(self):
         category_id = utils.create_uuid()
+        HTTPretty.register_uri(HTTPretty.POST,
+                               '%s/me/categories' % AnyDoAPIBinder.host,
+                               status=201)
         ret = self.api.create_category(name="ANY.DO_TEST_CATEGORY",
                                        default="false",
                                        isDefault="false",
                                        listPosition="null",
                                        id=category_id)
-        ret.status_code = 201
-        mock_class.return_value = ret
         self.assertEqual(ret.status_code, 201)
 
-    @patch('requests.adapters.HTTPAdapter.send')
-    def test_create_task(self, mock_class):
+    @httprettified
+    def test_create_task(self):
         category_id = utils.create_uuid()
         task_id = utils.create_uuid()
+        HTTPretty.register_uri(HTTPretty.POST,
+                               '%s/me/tasks' % AnyDoAPIBinder.host,
+                               status=200)
         ret = self.api.create_task(title="ANY.DO_TEST_TASK",
                                    listPositionByCategory=0,
                                    listPositionByPriority=0,
@@ -101,15 +114,15 @@ class AnyDoAPIBinderTests(unittest.TestCase):
                                    categoryId=str(category_id),
                                    dueDate=None,
                                    id=task_id)
-        ret.status_code = 200
-        mock_class.return_value = ret
         self.assertEqual(ret.status_code, 200)
 
-    @patch('requests.adapters.HTTPAdapter.send')
-    def test_create_note(self, mock_class):
+    @httprettified
+    def test_create_note(self):
         task_id = utils.create_uuid()
         category_id = utils.create_uuid()
         note_id = utils.create_uuid()
+        HTTPretty.register_uri(HTTPretty.POST,
+                               '%s/me/tasks' % AnyDoAPIBinder.host)
         # Add note in task
         ret = self.api.create_task(title="ANY.DO_TEST_NOTE",
                                    listPositionByCategory=0,
@@ -125,6 +138,4 @@ class AnyDoAPIBinderTests(unittest.TestCase):
                                    categoryId=str(category_id),
                                    dueDate=None,
                                    id=note_id)
-        ret.status_code = 200
-        mock_class.return_value = ret
         self.assertEqual(ret.status_code, 200)
