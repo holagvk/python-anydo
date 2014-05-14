15568d4f64e8eb7849dd81beae6603c6509124d8
Kouhei Maeda <mkouhei@palmtb.net>
2014-05-14 13:19:00 +0900

    Fixed pylint violations except of two Refactor of anydo.lib.bind.
    Not yet fixed as follows;
    ---
    Module anydo.lib.bind
    R: 15, 4: Too few public methods (1/2) (too-few-public-methods)
    R: 11, 0: Too many branches (18/12) (too-many-branches)
    
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- anydo/lib/settings.py
+++ anydo/lib/settings.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+""" anydo.lib.settings """
 PROXIES = None
 USERNAME = "username@example.org"
 PASSWORD = "password"
--- anydo/lib/utils.py
+++ anydo/lib/utils.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
+""" anydo.lib.utils """
 import sys
 import base64
 import uuid
 
 
 def encode_string(value):
+    """ encoding string
+    Returns: value | utf-8 encoded string
+    :param value: utf-8 encoded string or unicode
+    """
     if sys.version_info < (3, 0):
         return value.encode('utf-8') \
             if isinstance(value, unicode) else str(value)
@@ -13,6 +18,10 @@ def encode_string(value):
 
 
 def create_uuid():
+    """ creating uuid.
+    Returns: utf-8 encoded and base64 encoded uuid that is as follows;
+             e.g. u'08s-gOkKSWC26ntWUGkKIQ=='
+    """
     unique_id = base64.b64encode(uuid.uuid4().bytes)
     safe_id = unique_id.replace(b"+", b"-").replace(b"/", b"_")
     return safe_id.decode('utf-8')
